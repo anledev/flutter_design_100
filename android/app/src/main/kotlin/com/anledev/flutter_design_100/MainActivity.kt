eb0325ead28381d9e430932391379b099a84caa9
anlt <anlt@rikkeisoft.com>
2020-01-30 09:36:38 +0700

    init commit

---
+++ android/app/src/main/kotlin/com/anledev/flutter_design_100/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.anledev.flutter_design_100
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
